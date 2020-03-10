475de2d633d84fcdbcf6498fd689919bbd2771a0
Jaemo <jaemo.lee0204@gmail.com>
2020-03-10 00:11:48 -0700

    ML Pipeline Completion and Run to create train_classfier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 import nltk
 import re
 import pickle
+import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
@@ -16,25 +17,75 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import GridSearchCV
 
+import warnings
+warnings.filterwarnings("ignore")
+
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterMessage', engine)
+    # df = df.head(15)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    print (Y.head())
+    category_names = Y.columns
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    # get list of all urls using regex
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
 
 
 def build_model():
-    pass
+    '''
+    Function to build a model, create pipeline, hypertuning as well as gridsearchcv
+    Input: N/A
+    Output: Returns the model
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+
+    parameters = {
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100]
+        }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+    '''
+    Function to evaluate a model and return the classificatio and accurancy score.
+    Inputs: Model, X_test, y_test, Catgegory_names
+    Outputs: Prints the Classification report & Accuracy Score
+    '''
+    y_pred = model.predict(X_test)
+    print(classification_report(y_pred, Y_test.values, target_names=category_names))
+    # print raw accuracy score
+    print('Accuracy Score: {}'.format(np.mean(Y_test.values == y_pred)))
 
 def save_model(model, model_filepath):
-    pass
+    temp = open(model_filepath, 'wb')
+    pickle.dump(model, temp)
+    temp.close()
+
 
 
 def main():
